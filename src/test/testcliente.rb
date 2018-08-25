d021b8bebd6514e54ee8fa08674061b53933bbf2
MickeyCoreight <luis_mi1999@ciencias.unam.mx>
2018-08-25 15:13:43 -0500

    Se añadieron las pruebas unitarias: getUsuarios, getConexiones, Protocolo

--- src/test/testcliente.rb
+++ src/test/testcliente.rb
@@ -4,9 +4,19 @@ require "test/unit"
 
 class TestCliente < Test::Unit::TestCase
 
+	def test_getUsuarios
+	end
+
+	def test_getConexiones
+	end	
+
         def test_cliente
                 assert(true, "Prueba unitaria solo para ver que corra")
         end
+
+	def test_conecta
+		assert_not_equal("|=PROTOCOLO:")		
+	end
 end
 
 
