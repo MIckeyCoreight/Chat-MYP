938f430bf64c57921ddf67442c247914e2b531f4
MickeyCoreight <luis_mi1999@ciencias.unam.mx>
2018-08-26 18:53:32 -0500

    se añadieron las funciones para hacer que las pruebas unitarias corran, todo está a la espera de ser compilado

--- src/servidor.rb
+++ src/servidor.rb
@@ -1,21 +1,53 @@
 require 'socket'
 
-server = TCPServer.new 2000
-puts "servidor a la espera de clientes"	
-loop do
-  Thread.start(server.accept) do |client|
-    client.puts "|=PROTOCOLO:"
-    #client.puts "Time is #{Time.now}"
-    #recibe(client)
-    client.puts "salir"
-    client.close
+class Servidor
+
+  def intiliaze()
+      @servidor = TCPServer( 2000 , "localhost" )
+      @clientes = hash.new
+      @salas = hash.new
+      @conectados = 0
+      @contadorSalas = 0
+      @conexiones = hash.new
+      conexiones[:cliente] = @clientes
+      conexiones[:salas] = @salas
+      conexiones[:servidor] = @servidor
+      iniciar()
   end
 
-  def recibe(client)
-    recibido = client.gets
-    while recibido.equals?("salir")
-      puts recibido
-      recibido = client.gets
-    end  
+  def getConexiones
+      return @conexiones
   end
-end
+
+  def getSalas
+      return @salas
+  end
+
+  def getClientes
+       return @clientes
+  end
+
+  def creaSala(sala, socket)
+      if salas[sala] != nil
+        socket.puts "Sala ya existe"
+      else
+        salas[sala] = hash.new 
+      end 
+  end  
+
+  def unirseSala(usuario , sala)
+    if salas[sala] != nil
+      sala[sala][:cliente] = usuario
+    else
+      puts "no se pudo unir a la sala"
+    end
+  end
+
+  def registro(usuario)
+    if conexiones[:cliente][usuario] != nil
+      puts "el cliente ya existe"
+    else
+      conexiones[:cliente][usuario] = ususario
+  end  
+  
+end  
